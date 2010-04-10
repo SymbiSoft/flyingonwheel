259e2bd5cf1c249c297705ed426cca4893d6145c
skxiaonan <skxiaonan@gmail.com>
2010-04-10 15:18:05 +0000

    add mark_httplib.py for lower version python platform

--- Client/tpost.py
+++ Client/tpost.py
@@ -1,22 +1,26 @@
 import urllib2, urllib, sys
 import appuifw
+import httplib
 
 url = "http://flyingonwheel.appspot.com/mark_test"
 latitude = str('23.34')
 longitude = str('34.45')
 
 params = urllib.urlencode([('point', '%s, %s' % (latitude, longitude))])
-
-req = urllib2.Request(url)
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+conn = httplib.HTTPConnection("flyingonwheel.appspot.com")
+#req = urllib2.Request(url)
 try:
-    fd = urllib2.urlopen(req, params)
+    conn.request("POST", "/mark", params, headers)
 except:
+    conn.close()
     appuifw.note(u"Could not posting information", 'info')
 else:
+    conn.close()
     appuifw.note(u"Position Information Posted", 'info')
 
-while 1:
-    data = fd.read(1024)
-    if not len(data):
-        break
+#while 1:
+#    data = fd.read(1024)
+#    if not len(data):
+#        break
 #    sys.stdout.write(data)
