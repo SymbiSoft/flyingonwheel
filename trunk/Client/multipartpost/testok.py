b0c98c6623a9379d0a6fe8c8af6254fff16eb4cc
skxiaonan@gmail.com <skxiaonan@gmail.com@958c1779-2042-648a-5872-c01d9d7aaf4a>
2010-05-16 14:09:12 +0000

    update upload image from mobile

--- trunk/Client/multipartpost/testok.py
+++ trunk/Client/multipartpost/testok.py
@@ -2,10 +2,10 @@ import MultipartPostHandler, urllib2, cookielib
 
 latitude = str('23.34')
 longitude = str('34.45')
-file = 'D:\logo.jpg'
+file = 'logo.jpg'
 
 cookies = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookies), MultipartPostHandler.MultipartPostHandler)
 params = { 'point': '%s, %s' % (latitude, longitude), "picblob": open(file, "rb") }
-#params = urllib.urlencode([('point', '%s, %s' % (latitude, longitude)), ('picblob', '%s' % d)])
-opener.open("http://flyingonwheel.appspot.com/mark_test", params)
+answer = opener.open("http://flyingonwheel.appspot.com/mark_test", params)
+print answer.code
